7a8aa1ace8720784954312eb691d167eebaff146
sventekb24 <sventekb24@Z00T1-13>
2024-10-01 11:05:58 +0200

    Dokončene_cvičnylist

---
+++ Cvicenie_cykly/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class TestClass
+{
+    static void Main(string[] args)
+    {
+
+        int i = 0;
+        while (true)
+        {
+            Console.WriteLine(i);
+            i++;
+            if (i == 50000)
+            {
+                break;
+            }
+        }
+    }
+}
+
--- Cvičenie_List/Program.cs
+++ Cvičenie_List/Program.cs
@@ -1,20 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-static void Main(string[] args)
+﻿class TestClass
 {
-    List<string> listname = new List<string>(); 
+    static void Main(string[] args)
+    {
 
 
-    listname.Add("Branislav");
-    listname.Add("Alica");
-    listname.Add("Oliver");
-    listname.Add("Filip");
-    listname.Add("Lucka");
-    listname.Add("Sona");
 
-    Console.WriteLine(listname[0]);
-    Console.WriteLine(listname[1]);
-    Console.WriteLine(listname[2]);
+        List<string> listname = new List<string>();
 
 
-}
+        listname.Add("Branislav");
+        listname.Add("Alica");
+        listname.Add("Oliver");
+        listname.Add("Filip");
+        listname.Add("Lucka");
+        listname.Add("Sona");
+        {
+            int counter = 0;
+
+            foreach (string name in listname)
+            {
+                Console.WriteLine(counter + name.ToUpper());
+                counter++;
+            }
+        }
+        
+
+       foreach (string name in listname)
+        {
+            Console.WriteLine("Zadaj meno:");
+
+        }
+        
+    }
+
+
+    
+
+
+}  
+  
