291ea55dd95b6686ceedc83fe3ecc65e1bd614bf
sventekb24 <sventekb24@Z00T1-13>
2024-10-17 09:09:37 +0200

    15.10

--- Cvi4enie_OOP_Hra/Player.cs
+++ Cvi4enie_OOP_Hra/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,14 +13,56 @@ namespace Cvi4enie_OOP_Hra
         public int HP { get; set; }
         public int Mana { get; set; }
         public int AttackPower { get; set; }
+        public int Critchange { get; set; }
 
-        public void DamagePlayer (Player player)
+
+        public void DamagePlayer(Player player)
         {
+            int damageMultiPlayer = 1;
+
+            Random random = new Random();
+            int randomNumber = random.Next(0, 100);
+            if (randomNumber <= this.Critchange)
+            {
+                damageMultiPlayer = 2;
+                Console.WriteLine(this.Name + "dal krititcky zasah");
+            }
+
             int HPofEnemy = player.HP;
-            int AttackOfCurrentPlayer = this.AttackPower;
+            int AttackOfCurrentPlayer = this.AttackPower * damageMultiPlayer;
             int HpofEnemyAfterFight = HPofEnemy - AttackOfCurrentPlayer;
             player.HP = HpofEnemyAfterFight;
         }
 
+        public bool Heal()
+        {
+            if (this.Mana >= 0)
+            {
+
+
+
+                int HPOfPlayer = this.HP;
+                int ManaOfPlayer = this.Mana;
+                int HPOfPlayerAfterHeal = HPOfPlayer + ManaOfPlayer;
+                this.Mana = 0;
+                this.HP = HPOfPlayer;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+            public void RefilMana(int newMana)
+            {
+                this.Mana = newMana;
+            Random random = new Random();
+            int randomNumber = random.Next(0, 100);
+            if (randomNumber <= this.Mana)
+                 {
+                    Console.WriteLine(this.Mana + " zasah ");
+                 }
+            }
+        
     }
 }
--- Cvi4enie_OOP_Hra/Program.cs
+++ Cvi4enie_OOP_Hra/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Cvi4enie_OOP_Hra
+﻿using System.Diagnostics.SymbolStore;
+
+namespace Cvi4enie_OOP_Hra
 {
     internal class Program
     {
@@ -10,6 +12,7 @@
                 player.HP = 100;
                 player.Mana = 8;
                 player.AttackPower = 6;
+                player.Critchange = 5;
             }
             Player player2 = new Player();
             {
@@ -17,25 +20,55 @@
                 player2.HP = 100;
                 player2.Mana = 15;
                 player2.AttackPower = 3;
+                player2.Critchange = 4;
             }
-            Console.WriteLine("Zaporna postava HP:" + player2 );
-            player.DamagePlayer(player2);
-            Console.WriteLine("Zaporna postava HP:" + player2);
-            
-            int HP = 100;
-            while (true)
-            {
-                if (player.HP > 15)
+
+            while (player.HP >= 0 && player2.HP >= 0)
+                while (player.HP >= 0 && player2.HP >= 0)
                 {
-                    return player.Mana++;
-                }
-                else if (player2.HP < 10)
-                { 
-                    return player2.Mana++;
-                }
-            }
+                    Console.WriteLine("player:" + player.HP);
+                    Console.WriteLine("player2:" + player.HP);
+                    player.DamagePlayer(player2);
+                    player2.DamagePlayer(player);
+
+                    if (player.HP <= 20)
+                    {
+                        bool wasHealed = player.Heal();
+                        if (wasHealed)
+                        {
+                            Console.WriteLine("player postava bola uzdravena.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("player nema manu nebola uzdravena.");
+                        }
+
+                    }
+                    if (player2.HP <= 20)
+                    {
+                        bool wasHealed = player2.Heal();
+                        if (wasHealed)
+                        {
+                            Console.WriteLine("player2 postava bola uzdravena.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("player2 nema manu nebola uzdravena.");
+                        }
+
+                    }
+                    player.RefilMana(10);
+                    Random random = new Random();
+                    int randomNumber = random.Next(0, 100);
+                    if (randomNumber <= 5)
+                    {
+                        player.RefilMana(10);
+                        Console.WriteLine("mana bola dobita");
+                    }
+                                    
 
 
+                }
         }
     }
 }
