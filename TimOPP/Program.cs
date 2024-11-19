3ceec5f490c1abeb5aa6932dace33e8728286976
sventekb24 <sventekb24@Z00T1-13>
2024-11-19 10:11:50 +0100

    TimOPP

---
+++ TimOPP/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace TimOPP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tim tim = new Tim();
+
+            tim.PocetT = (5);
+            tim.nazov = "ziaci";
+
+            Console.WriteLine("Pocet:" + tim.PocetT);
+            Console.WriteLine("Nazov:" + tim.nazov);
+        }
+    }
+}
---
+++ TimOPP/Tim.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimOPP
+{
+    public class Tim
+    {
+        private int PocetT;
+        private string nazov;
+
+        public Tim()
+        { 
+        } 
+
+        public Tim (int PocetT,  string nazov)
+        {
+            this.PocetT = PocetT;
+            this.nazov = nazov;
+        }
+        public int PocetT
+        {
+            get { return PocetT; }
+            set { PocetT = value; }
+        }
+        public string nazov
+        {
+            get { return nazov; }
+            set { nazov = value; }
+        }
+    }      
+    
+}
