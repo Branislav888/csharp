d5686d9997428a458d3d747308e0755fa75884b3
sventekb24 <sventekb24@Z00T1-13>
2024-11-28 08:15:49 +0100

    hadyARebriky

--- hadyARebriky/Program.cs
+++ hadyARebriky/Program.cs
@@ -5,15 +5,15 @@
         static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("HHH       HHH           AAA           DDDDDDDD      YYYY         YYY");
-            Console.WriteLine("HHH       HHH          AAAAA          DDD    DDD     YYY        YYY");
-            Console.WriteLine("HHH       HHH         AAA AAA         DDD     DDD     YYY      YYY");
-            Console.WriteLine("HHH       HHH        AAA   AAA        DDD      DDD     YYY    YYY");
-            Console.WriteLine("HHHHHHHHHHHHH       AAAAAAAAAAA       DDD       DDD     YYY  YYY");
-            Console.WriteLine("HHHHHHHHHHHHH      AAAAAAAAAAAAA      DDD       DDD      YYYYY");
-            Console.WriteLine("HHH       HHH     AAA         AAA     DDD      DDD        YYY");
-            Console.WriteLine("HHH       HHH    AAA           AAA    DDD     DDD         YYY");
-            Console.WriteLine("HHH       HHH   AAA             AAA   DDDDDDDD            YYY");
+            Console.WriteLine("     HHH       HHH           AAA           DDDDDDDD      YYYY         YYY");
+            Console.WriteLine("     HHH       HHH          AAAAA          DDD    DDD     YYY        YYY");
+            Console.WriteLine("     HHH       HHH         AAA AAA         DDD     DDD     YYY      YYY");
+            Console.WriteLine("     HHH       HHH        AAA   AAA        DDD      DDD     YYY    YYY");
+            Console.WriteLine("     HHHHHHHHHHHHH       AAAAAAAAAAA       DDD       DDD     YYY  YYY");
+            Console.WriteLine("     HHHHHHHHHHHHH      AAAAAAAAAAAAA      DDD       DDD      YYYYY");
+            Console.WriteLine("     HHH       HHH     AAA         AAA     DDD      DDD        YYY");
+            Console.WriteLine("     HHH       HHH    AAA           AAA    DDD     DDD         YYY");
+            Console.WriteLine("     HHH       HHH   AAA             AAA   DDDDDDDD            YYY");
             Console.WriteLine();
             Console.WriteLine("                                 AAA");
             Console.WriteLine("                                AAAAA");
