6ab7ceac776e28cfff73b6bbef51ee6fa8eed358
sventekb24 <sventekb24@Z00T1-13>
2025-02-04 11:04:48 +0100

    PokemonGame

---
+++ PokemonGame/GameEngine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonGame
+{
+    public class GameEngine
+    {
+        public Pokemon FirstPokemon { get; set; }   
+        public Pokemon SecondPokemon { get; set; }
+
+        public GameEngine() 
+        {
+            FirstPokemon = new Pokemon(name: "Pikachu"); 
+            SecondPokemon = new Pokemon(name: "Charizard");
+        }
+    }
+}
---
+++ PokemonGame/Pokemon.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonGame
+{
+    public class Pokemon
+    {
+        public string Meno { get; set; }
+        public int Health { get; set; }
+
+        public Pokemon(string meno, int health)
+        {
+            this.Meno = meno;
+            this.Health = health;
+        }
+        public int Attack1()
+        { 
+            Random random = new Random();
+            return random.Next(10, 91);
+
+        }
+        public int Attack2()
+        {
+            Random random = new Random();
+            return random.Next(30, 71);
+        }
+        public int Attack3()
+        {
+            Random random = new Random();
+            return random.Next(40, 61);
+        }
+        public int Heal()
+        {
+            Random random = new Random();
+            int healValue = random.Next(10, 91);
+            healValue += healValue;
+            if (Health > 100)
+            {
+                Health = 100;
+            }
+            return healValue;
+        }
+    }
+}
